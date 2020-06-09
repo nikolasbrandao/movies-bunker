d65e9a77833efd84ae20029db9cf7869e3743a23
nikolasbrandao <nikolasbrandao@gmail.com>
2020-06-09 02:23:39 -0300

    feat: add a redux to favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,14 +1,38 @@
 import React from "react";
-import { Container, Title, SubTitle } from "../../components";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Title, SubTitle, CardMovie } from "../../components";
+import { addMovie, removeMovie } from "../../redux/Movies";
 import * as S from "./styles";
 
-const FavoritesPage = () => (
-  <Container>
-    <S.TitleWrapper>
-      <Title>Favoritos</Title>
-      <SubTitle>Seus filmes prediletos.</SubTitle>
-    </S.TitleWrapper>
-  </Container>
-);
+const FavoritesPage = () => {
+  const movies = useSelector((state) => state.moviesReducer);
+  const dispatch = useDispatch();
+
+  const handleFavoriteButton = (item) => {
+    const newMovie = { ...item, Favorite: !item.Favorite };
+    if (newMovie.Favorite) {
+      dispatch(addMovie(newMovie));
+    } else {
+      dispatch(removeMovie(newMovie.imdbID));
+    }
+  };
+  return (
+    <Container>
+      <S.TitleWrapper>
+        <Title>Favoritos</Title>
+        <SubTitle>Seus filmes prediletos.</SubTitle>
+      </S.TitleWrapper>
+      <S.ResultMoviesWrapper>
+        {movies.map((movie) => (
+          <CardMovie
+            key={movie.imdbID}
+            movie={movie}
+            onPressFavorite={handleFavoriteButton}
+          />
+        ))}
+      </S.ResultMoviesWrapper>
+    </Container>
+  );
+};
 
 export default FavoritesPage;
--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -5,3 +5,16 @@ export const TitleWrapper = styled.header`
   flex-direction: column;
   margin: 2em 1em 1em 1em;
 `;
+
+export const ResultMoviesWrapper = styled.div`
+  height: 65vh;
+  width: 92%;
+  margin: 4%;
+  padding-left: 1%;
+  padding-right: 1%;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+`;
